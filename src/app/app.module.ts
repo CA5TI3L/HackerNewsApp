4eeebd4939d120d880a917c44b23a7476d717358
Kevin Terblanche <KTerblanche@jhb.dvt.co.za>
2020-11-04 13:48:13 +0200

    Header styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { EffectsModule } from '@ngrx/effects';
+import { NewsEffects } from './store/effects/news.effects';
+import * as fromNews from './store/reducers/news.reducer';
+import { reducer } from './store/reducers/news.reducer';
+
+registerLocaleData(en);
+
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    console.log('state', state);
+    console.log('action', action);
+    return reducer(state, action);
+  };
+}
 
 @NgModule({
   declarations: [
@@ -12,9 +36,17 @@ import { StoreModule } from '@ngrx/store';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({newsState: reducer}, { metaReducers: [debug] }),
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule,
+    NzIconModule,
+    NzTypographyModule,
+    EffectsModule.forRoot([]),
+    EffectsModule.forFeature([NewsEffects])
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
